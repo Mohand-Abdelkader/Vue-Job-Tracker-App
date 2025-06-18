4212f056c0b8462adaf24f71ba7cdd1e37b36ca2
mohandAbdelkader <mohand27m@gmail.com>
2025-06-19 00:05:29 +0300

    setup header and sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/ui/Header.vue";
+import Sidebar from "./components/ui/Sidebar.vue";
 </script>
 
 <template>
-  <p>Navbar</p>
-  <router-view />
-  <p>footer</p>
+  <div class="flex h-screen overflow-hidden">
+    <aside class="w-64 flex-shrink-0">
+      <Sidebar />
+    </aside>
+
+    <div class="flex flex-col flex-1 overflow-auto">
+      <header>
+        <Header />
+      </header>
+      <main class="flex-1 p-6 bg-gray-100 overflow-auto">
+        <router-view />
+      </main>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/ui/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header className="bg-white">
+    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 items-center justify-between">
+        <div className="flex-1 md:flex md:items-center md:gap-12">
+          <h2 className=" text-2xl font-bold text-blue-950">Job-Tracker-App</h2>
+        </div>
+
+        <div className="md:flex md:items-center md:gap-12">
+          <div className="hidden md:relative md:block">
+            <div
+              className="overflow-hidden rounded-full border border-gray-300 shadow-inner"
+            >
+              <img
+                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="size-10 object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
